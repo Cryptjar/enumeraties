6bad8883739a0111f792bb72442ecbf3ee782965
Cryptjar <cryptjar@junk.studio>
2022-01-18 22:23:32 +0100

    Reformat macros to use tabs for indentation

--- src/lib.rs
+++ src/lib.rs
@@ -28,14 +28,15 @@ props! {
 }
 */
 
-// The public front-end macro (idk about how to cfg the lazy branches in this approach)
-/// Adding a property to an enum
+// The public front-end macro
+
+/// Adds a property onto an enum
 ///
 /// Basic syntax with deref:
 ///
 /// ```text
 /// impl Deref for <ENUM> as (lazy|const) <PROPERTY> {
-///     <VARINT> => {
+///     <VARIANT> => {
 ///         <FIELD> : <VALUE>,
 ///         ...
 ///     },
@@ -47,7 +48,7 @@ props! {
 ///
 /// ```text
 /// impl <ENUM> : <VIS> fn <FN_NAME> as (lazy|const) <PROPERTY> {
-///     <VARINT> => {
+///     <VARIANT> => {
 ///         <FIELD> : <VALUE>,
 ///         ...
 ///     },
@@ -56,153 +57,154 @@ props! {
 /// ```
 #[macro_export]
 macro_rules! props {
-    (
-        // A lazy/const impl
-        impl Deref for $enum_name:ty as $modifier:ident $prop_name:path { $($matching:tt)* }
-    ) => {
-        // Add the EnumProp impl
-        $crate::props_internal_impl!{
-            @EnumProp
-            mod($modifier) ($prop_name) for $enum_name {
-                $($matching)*
-            }
-        }
+	(
+		// A lazy/const impl
+		impl Deref for $enum_name:ty as $modifier:ident $prop_name:path { $($matching:tt)* }
+	) => {
+		// Add the EnumProp impl
+		$crate::internal_props_impl_macro!{
+			@EnumProp
+			mod($modifier) ($prop_name) for $enum_name {
+				$($matching)*
+			}
+		}
 
-        // Add the deref forwarding
-        impl $crate::Deref for $enum_name {
-            type Target = $prop_name;
-            fn deref(&self) -> &Self::Target {
-                $crate::EnumProp::<$prop_name>::property(self)
-            }
-        }
-    };
-    (
-        // The lazy/const impl that will be promoted to `Deref` (also impls `EnumProp`)
-        impl $enum_name:ty : $fn_vis:vis fn $fn_name:ident as $modifier:ident $prop_name:path { $($matching:tt)* }
-    ) => {
-        // Add the EnumProp impl
-        $crate::props_internal_impl!{
-            @EnumProp
-            mod($modifier) ($prop_name) for $enum_name {
-                $($matching)*
-            }
-        }
+		// Add the deref forwarding
+		impl $crate::Deref for $enum_name {
+			type Target = $prop_name;
+			fn deref(&self) -> &Self::Target {
+				$crate::EnumProp::<$prop_name>::property(self)
+			}
+		}
+	};
+	(
+		// The lazy/const impl that will be promoted to `Deref` (also impls `EnumProp`)
+		impl $enum_name:ty : $fn_vis:vis fn $fn_name:ident as $modifier:ident $prop_name:path { $($matching:tt)* }
+	) => {
+		// Add the EnumProp impl
+		$crate::internal_props_impl_macro!{
+			@EnumProp
+			mod($modifier) ($prop_name) for $enum_name {
+				$($matching)*
+			}
+		}
 
-        // Add the inherent method forwarding
-        impl $enum_name {
-            $fn_vis fn $fn_name(&self) -> &'static $prop_name {
-                $crate::EnumProp::<$prop_name>::property(self)
-            }
-        }
-    };
+		// Add the inherent method forwarding
+		impl $enum_name {
+			$fn_vis fn $fn_name(&self) -> &'static $prop_name {
+				$crate::EnumProp::<$prop_name>::property(self)
+			}
+		}
+	};
 }
 
-// The interal marco impl, used by `props`
+// The internal marco impl, used by `props`, do not use, its API may change
+// at any time
 #[doc(hidden)]
 #[macro_export]
-macro_rules! props_internal_impl {
-    (
-        // The enum prop impl, entry rule
-        @EnumProp
+macro_rules! internal_props_impl_macro {
+	(
+		// The enum prop impl, entry rule
+		@EnumProp
 
-        // Just some fancy header, we could add `Deref` somewhere to make clear
-        // that that's what we actually implement
-        mod($modifier:ident) ($prop_name:path) for $enum_name:ty {
-            $(
-                // True match branches, could be simplified to `ident`, but then
-                // one can on longer identify e.g. `Beta(42)` (maybe one shouldn't)
-                $branch:pat => {
-                    $(
-                        $struct_fields:tt
-                    )*
-                } $(,)?
-            )*
-        }
-    ) => {
-        impl $crate::EnumProp<$prop_name> for $enum_name {
-            fn property(&self) -> &'static $prop_name {
-                match self {
-                    $(
-                        $branch => {
-                            $crate::props_internal_impl!(
-                                @Branch mod($modifier) $prop_name {
-                                    $( $struct_fields )*
-                                }
-                            )
-                        },
-                    )*
-                }
-            }
-        }
-    };
+		// Just some fancy header, we could add `Deref` somewhere to make clear
+		// that that's what we actually implement
+		mod($modifier:ident) ($prop_name:path) for $enum_name:ty {
+			$(
+				// True match branches, could be simplified to `ident`, but then
+				// one can on longer identify e.g. `Beta(42)` (maybe one shouldn't)
+				$branch:pat => {
+					$(
+						$struct_fields:tt
+					)*
+				} $(,)?
+			)*
+		}
+	) => {
+		impl $crate::EnumProp<$prop_name> for $enum_name {
+			fn property(&self) -> &'static $prop_name {
+				match self {
+					$(
+						$branch => {
+							$crate::internal_props_impl_macro!(
+								@Branch mod($modifier) $prop_name {
+									$( $struct_fields )*
+								}
+							)
+						},
+					)*
+				}
+			}
+		}
+	};
 
-    (
-        // A single *const* prop value
-        @Branch
+	(
+		// A single *const* prop value
+		@Branch
 
-        // Just some fancy header, we could add `Deref` somewhere to make clear
-        // that that's what we actually implement
-        mod(const) $prop_name:path {
-            $(
-                $field:ident : $value:expr
-            ),* $(,)?
-        }
-    ) => {{
-        // A static reference given, that all `$value`s are const-init
-        // Notice the difference between `static` and `const` here:
-        // Both need const-init data, but only `static` requires `Send`, i.e.
-        // `const` has less requirements, both work here.
-        //
-        // However, with `const` it is possible that two invocations of the
-        // sourounding function return different references (i.e. different
-        // addresses) because const gets inlined.
-        //
-        // If this suddle difference is important, it is concivable to present
-        // both a `const` and a `static` variant.
-        //
-        // So, it just should be pointed out somewhere (in the docs).
+		// Just some fancy header, we could add `Deref` somewhere to make clear
+		// that that's what we actually implement
+		mod(const) $prop_name:path {
+			$(
+				$field:ident : $value:expr
+			),* $(,)?
+		}
+	) => {{
+		// A static reference given, that all `$value`s are const-init
+		// Notice the difference between `static` and `const` here:
+		// Both need const-init data, but only `static` requires `Send`, i.e.
+		// `const` has less requirements, both work here.
+		//
+		// However, with `const` it is possible that two invocations of the
+		// surrounding function return different references (i.e. different
+		// addresses) because const gets inlined.
+		//
+		// If this subtle difference is important, it is conceivable to present
+		// both a `const` and a `static` variant.
+		//
+		// So, it just should be pointed out somewhere (in the docs).
 
-        // Notice, having the explicit constant/static gives clearer error messages.
+		// Notice, having the explicit constant/static gives clearer error messages.
 
-        // `BAR` is rather arbitrary here, maybe different name would be better
-        const BAR : $prop_name = {
-            $prop_name {
-                $(
-                    $field : $value ,
-                )*
-            }
-        };
+		// `BAR` is rather arbitrary here, maybe different name would be better
+		const BAR : $prop_name = {
+			$prop_name {
+				$(
+					$field : $value ,
+				)*
+			}
+		};
 
-        & BAR
-    }};
+		& BAR
+	}};
 
-    (
-        // A single *const* prop value
-        @Branch
+	(
+		// A single *const* prop value
+		@Branch
 
-        // Just some fancy header, we could add `Deref` somewhere to make clear
-        // that that's what we actually implement
-        mod(lazy) $prop_name:path {
-            $(
-                $field:ident : $value:expr
-            ),* $(,)?
-        }
-    ) => {{
-        // A static reference via lazy_static.
+		// Just some fancy header, we could add `Deref` somewhere to make clear
+		// that that's what we actually implement
+		mod(lazy) $prop_name:path {
+			$(
+				$field:ident : $value:expr
+			),* $(,)?
+		}
+	) => {{
+		// A static reference via lazy_static.
 
-        // `FOO` is rather arbitrary here, maybe different name would be better
-        $crate::lazy_static::lazy_static!{
-            static ref FOO: $prop_name = {
-                $prop_name {
-                    $(
-                        $field : $value ,
-                    )*
-                }
-            };
-        }
+		// `FOO` is rather arbitrary here, maybe different name would be better
+		$crate::lazy_static::lazy_static!{
+			static ref FOO: $prop_name = {
+				$prop_name {
+					$(
+						$field : $value ,
+					)*
+				}
+			};
+		}
 
-        &*FOO
-    }};
+		&*FOO
+	}};
 }
 
 
